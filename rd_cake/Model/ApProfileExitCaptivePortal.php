03fe0690ae25aa1761cf8f4bfd5c9447502de56d
wifimedia <dungtd8x@gmail.com>
2018-12-13 16:09:13 +0700

    vvvvv

---
+++ rd_cake/Model/ApProfileExitCaptivePortal.php
@@ -0,0 +1,56 @@
+<?php
+App::uses('AppModel', 'Model');
+
+class ApProfileExitCaptivePortal extends AppModel {
+
+     public $belongsTo = array(
+        'ApProfileExit' => array(
+                    'className' => 'ApProfileExit',
+                    'foreignKey' => 'ap_profile_exit_id'
+                    ),
+        'ApProfileExitUpstream' => [
+                'className'     => 'ApProfileExit',
+                'foreignKey'    => 'ap_profile_exit_upstream_id'
+            ]
+        );
+
+     public $validate = array(
+        'radius_nasid' => array(
+            'required' => array(
+                'rule' => array('notBlank'),
+                'message' => 'Value is required'
+            ),
+            'unique' => array(
+                'rule'    => 'isUnique',
+                'message' => 'This value is already taken'
+            )
+        ),
+        'radius1' => array(
+            'required' => array(
+                'rule' => array('notBlank'),
+                'message' => 'Value is required'
+            )
+        ),
+        'secret' => array(
+            'required' => array(
+                'rule' => array('notBlank'),
+                'message' => 'Value is required'
+            )
+        ),
+        'uam_url' => array(
+            'required' => array(
+                'rule' => array('notBlank'),
+                'message' => 'Value is required'
+            )
+        ),
+        'uam_secret' => array(
+            'required' => array(
+                'rule' => array('notBlank'),
+                'message' => 'Value is required'
+            )
+        )   
+    );
+
+}
+
+?>
